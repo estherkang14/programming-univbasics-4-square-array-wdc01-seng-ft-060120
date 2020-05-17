2f284eb3a8bf0a81846f34e11441242faf600f8c
Esther Kang <estherkang14@gmail.com>
2020-05-17 22:27:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,18 +1,9 @@
-#def square_array(array)
-#  new_array = []
-#  
-#  array.length.times do |index|
-#    new_array.push(array[index] ** 2)
-#  end
-#  new_array
-#end
-
 def square_array(array)
-  count = 0 
   new_array = []
   
-  while array[count] do
-    new_array.push(array[count] ** 2)
+  array.length.times do |index|
+    new_array.push(array[index] ** 2)
   end
   new_array
 end
+
