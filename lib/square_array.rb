90ecd5c696652ddbe2250b69bbddfee01740eab8
Esther Kang <estherkang14@gmail.com>
2020-05-17 22:23:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 #def square_array(array)
 #  new_array = []
-  
+#  
 #  array.length.times do |index|
 #    new_array.push(array[index] ** 2)
 #  end
 #  new_array
-end
+#end
 
 def square_array(array)
   count = 0 
